8deb1588355ec2333cf4f5298fb12722f48dd838
AFFogarty <andrew.f.fogarty@gmail.com>
2016-03-23 16:56:25 -0400

    New: Short code field no longer filters out words

--- public/classifier/models/fields/short_code_field.py
+++ public/classifier/models/fields/short_code_field.py
@@ -19,43 +19,5 @@ def sanitize_short_code(input):
     """
     # Turn spaces and dots into single dots
     new_code = re.sub(duplicate_spaces_and_dots, '.', input.strip().lower())
-    # Filter out everything bad
-    new_code = replace_common_words(re.sub(unacceptable_chars, '', new_code))
     # Duplicates once more
     return re.sub(duplicate_spaces_and_dots, '.', new_code)
-
-def replace_common_words(input):
-    # Neumes that we will shorten
-    replacements = [
-        ("torculus", "torc"),
-        ("tractulus", "trac"),
-        ("punctum", "pun"),
-        ("stropha", "stro"),
-        ("virga", "vir"),
-        ("porrectus", "por"),
-        ("ancus", "anc"),
-        ("status", "stra"),
-        ("quadratus", "q"),
-        ("quassus", "quas"),
-        ("oriscus", "ori"),
-        ("episema", "e"),
-        ("clivis", "cli"),
-        ("rotundus", "r"),
-        ("liquescent", "l"),
-        ("quilismapes", "pes.quil"),
-        ("two", "2"),
-        ("three", "3"),
-        # Important to strip simple
-        (".simple", ""),
-        # Some other language stuff
-        ("langer", "long"),
-        (".zweiter", ""),
-        (".abstrich", "")
-    ]
-    return replace_words(input, replacements)
-
-def replace_words(input, replacements):
-    for replacement in replacements:
-        old, new = replacement
-        input = re.sub(old, new, input)
-    return input
