64f559a268d65a29a5db3c480ad733230c055d13
AFFogarty <andrew.f.fogarty@gmail.com>
2016-03-23 16:57:53 -0400

    Fixed: MEI importer no longer crashes the application

--- public/classifier/helpers/importers/mei_importer.py
+++ public/classifier/helpers/importers/mei_importer.py
@@ -1,8 +1,6 @@
 from classifier.helpers.importers.abstract_importer import AbstractImporter
 from classifier.helpers.mei import MEI, get_st_gallen_390_image_url
-from classifier.models.name import Name
-from classifier.models.glyph import get_or_create_glyph
-from classifier.models.image import Image
+from classifier.models.glyph import Glyph
 
 
 class MeiImporter(AbstractImporter):
@@ -17,42 +15,41 @@ class MeiImporter(AbstractImporter):
                 print("NONE ERROR")
                 continue
             # Check if the name already exists
-            name, name_created = Name.objects.get_or_create(string=neume.name)
-            glyph, glyph_created = get_or_create_glyph(neume.name)
+            glyph = Glyph(neume.name)
             # Assign the new glyph to the name and save the name
-            name.glyph = glyph
-            name.save()
+            # name.glyph = glyph
+            # name.save()
             # Create the image
-            self.create_image(glyph, neume)
-
-    def create_image(self, glyph, neume):
-        '''
-        Create an image for the particular neume.
-
-        :param glyph:
-        :param neume:
-        :return:
-        '''
-        #pil_image = get_image(
-        #    get_st_gallen_390_image_url(self.file_name, neume.ulx, neume.uly,
-        #                                neume.get_width(), neume.get_height()))
-        external_image = get_st_gallen_390_image_url(self.file_name, neume.ulx, neume.uly, neume.get_width(), neume.get_height())
-        # print external_image
-        # Create Image model
-        image = Image()
-        image.glyph = glyph
-        #image.set_PIL_image(pil_image)
-        image.external_image = external_image
-
-        image.set_md5()
-        image.ulx = neume.ulx
-        image.uly = neume.uly
-        image.folio_name = self.file_name
-        image.width = neume.get_width()
-        image.height = neume.get_height()
-        # Make sure not duplicate
-        if not Image.objects.filter(glyph=glyph, md5sum=image.md5sum):
-            image.save()
+    #         self.create_image(glyph, neume)
+    #
+    # def create_image(self, glyph, neume):
+    #     """
+    #     Create an image for the particular neume.
+    #
+    #     :param glyph:
+    #     :param neume:
+    #     :return:
+    #     """
+    #     #pil_image = get_image(
+    #     #    get_st_gallen_390_image_url(self.file_name, neume.ulx, neume.uly,
+    #     #                                neume.get_width(), neume.get_height()))
+    #     external_image = get_st_gallen_390_image_url(self.file_name, neume.ulx, neume.uly, neume.get_width(), neume.get_height())
+    #     # print external_image
+    #     # Create Image model
+    #     image = Image()
+    #     image.glyph = glyph
+    #     #image.set_PIL_image(pil_image)
+    #     image.external_image = external_image
+    #
+    #     image.set_md5()
+    #     image.ulx = neume.ulx
+    #     image.uly = neume.uly
+    #     image.folio_name = self.file_name
+    #     image.nrows = neume.get_width()
+    #     image.ncols = neume.get_height()
+    #     # Make sure not duplicate
+    #     if not Image.objects.filter(glyph=glyph, md5sum=image.md5sum):
+    #         image.save()
 
 
 def import_mei_file(file_path):
