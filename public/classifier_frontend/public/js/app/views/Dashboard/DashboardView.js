f0b27b1ecf54553b920fc1b71af0ef84d399990b
AFFogarty <andrew.f.fogarty@gmail.com>
2016-03-24 17:53:07 -0400

    New: Dashboard can launch the glyph editor

--- public/classifier_frontend/public/js/app/views/Dashboard/DashboardView.js
+++ public/classifier_frontend/public/js/app/views/Dashboard/DashboardView.js
@@ -1,13 +1,14 @@
 import Marionette from "marionette";
-import Glyph from "models/Glyph";
+import GlyphEvents from "events/GlyphEvents";
 import GlyphCollection from "collections/GlyphCollection";
-import GlyphClassCollection from "collections/GlyphClassCollection";
 import ClassTreeView from "views/ClassTree/ClassTreeView";
 import GlyphTableView from "views/GlyphTable/GlyphTableView";
-import CreateClassView from "views/ClassTree/CreateClassView";
+import GlyphEditView from "views/GlyphEdit/GlyphEditView";
+import GlyphEditViewModel from "views/GlyphEdit/GlyphEditViewModel";
 import ModalViewModel from "views/widgets/Modal/ModalViewModel";
 import ModalView from "views/widgets/Modal/ModalView";
 import template from "./dashboard.template.html";
+
 
 export default Marionette.LayoutView.extend({
     template,
@@ -21,11 +22,13 @@ export default Marionette.LayoutView.extend({
 
     onShow: function()
     {
-        //var classCollection = new GlyphClassCollection();
-        //
-        //classCollection.add({
-        //    shortcode: "Test!"
-        //});
+        // var modalViewModel = new ModalViewModel({
+        //     title: "Load GameraXML File",
+        //     innerView: test,
+        //     isCloseable: true
+        // });
+        // var modal = new ModalView({model: modalViewModel});
+        // this.modalTestRegion.show(modal);
 
         var glyphCollection = new GlyphCollection();
         glyphCollection.add(this.model.get("glyph_set"));
@@ -33,20 +36,26 @@ export default Marionette.LayoutView.extend({
         // Show the tree
         this.glyphTreeRegion.show(new ClassTreeView({collection: glyphCollection}));
         // Show the glyph table
-        this.glyphTableRegion.show(new GlyphTableView({collection: glyphCollection}));
-        // Show the creation view
-        this.classCreateRegion.show(new CreateClassView({collection: classCollection}));
+        var glyphTable = new GlyphTableView({collection: glyphCollection});
+        this.glyphTableRegion.show(glyphTable);
 
-
-        var test = new ClassTreeView({collection: classCollection});
-        var modalViewModel = new ModalViewModel({
-            title: "Load GameraXML File",
-            innerView: test,
-            isCloseable: true
+        var that = this;
+        glyphTable.on(GlyphEvents.openGlyphEdit, function(model)
+        {
+            console.log("Open glyphedit!", model);
+            var modal = new ModalView({
+                model: new ModalViewModel({
+                    title: "Edit Glyph",
+                    isCloseable: true,
+                    innerView: new GlyphEditView({model: new GlyphEditViewModel({model: model})})
+                })
+            });
+            that.modalTestRegion.show(modal);
+            modal.open();
         });
-        console.log(modalViewModel);
-        var modal = new ModalView({model: modalViewModel});
-        this.modalTestRegion.show(modal);
+        // Show the creation view
+        //this.classCreateRegion.show(new CreateClassView({collection: classCollection}));
+
         //modal.open();
     }
 });
