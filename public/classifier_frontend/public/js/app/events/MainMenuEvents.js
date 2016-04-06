6acadd9ed40ad4f731379c784c3c93da7b63ea05
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-06 14:06:52 -0400

    New: Some more event types

---
+++ public/classifier_frontend/public/js/app/events/MainMenuEvents.js
@@ -0,0 +1,6 @@
+export default {
+    clickFileOpen: "file:open",
+    clickFileImport: "file:import",
+    clickClassifierGuessAll: "classifier:guess:all",
+    clickClassifierResetAll: "classifier:reset:all"
+}
