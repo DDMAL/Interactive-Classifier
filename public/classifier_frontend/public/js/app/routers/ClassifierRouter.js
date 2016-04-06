0bfdce12033d318f0bca5898dd6b8b6aaa952ff2
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-06 14:08:09 -0400

    Fixed: Router cleanup

--- public/classifier_frontend/public/js/app/routers/ClassifierRouter.js
+++ public/classifier_frontend/public/js/app/routers/ClassifierRouter.js
@@ -19,39 +19,4 @@ export default Marionette.AppRouter.extend({
     {
         App.editPage(id);
     }
-
-    //openGlyphList: function()
-    //{
-    //    // Start the glyph list module
-    //    App.module("GlyphList").start();
-    //},
-    //
-    //openGlyphEditor: function(id)
-    //{
-    //    // Start the individual glyph editor
-    //    App.module("GlyphEdit").start();
-    //    App.module("GlyphEdit").initializeId(id);
-    //},
-    //
-    //openNomenclatureList: function()
-    //{
-    //    // Start the nomenclature list module
-    //    App.module("NomenclatureList").start();
-    //},
-    //
-    //openNomenclatureEdit: function(id)
-    //{
-    //    App.module("NomenclatureEdit").start();
-    //    App.module("NomenclatureEdit").initializeId(id);
-    //},
-
-    // routeToPage: function(url)
-    // {
-    //     var newPageUrl = GlobalVars.SITE_SUBFOLDER + String(url).replace(/.*\/neumeeditor\//g, "");
-    //     this.navigate(
-    //         // Strip site url if need be
-    //         newPageUrl,
-    //         {trigger: true}
-    //     );
-    // }
 });
