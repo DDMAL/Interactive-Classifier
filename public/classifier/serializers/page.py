aaa4dd3fab17441cfcfb839d9df79a4500605f80
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-06 14:03:29 -0400

    New: serialize page owner

--- public/classifier/serializers/page.py
+++ public/classifier/serializers/page.py
@@ -2,11 +2,13 @@ from classifier.models.page import Page
 from rest_framework import serializers
 
 from classifier.serializers.glyph import GlyphSerializer
+from classifier.serializers.user import UserSerializer
 
 
 class PageFullSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
     glyph_set = GlyphSerializer(many=True, read_only=True)
+    owner = UserSerializer(read_only=True)
 
     class Meta:
         model = Page
@@ -14,5 +16,7 @@ class PageFullSerializer(serializers.HyperlinkedModelSerializer):
 
 class PageTeaserSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    owner = UserSerializer(read_only=True)
     class Meta:
         model = Page
+
