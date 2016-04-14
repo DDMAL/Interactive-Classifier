657df2c9e48dea3a21361c9e63fc266bdaae28f5
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-14 19:27:44 -0400

    New: Page stats view

---
+++ public/classifier_frontend/public/js/app/views/PageStats/PageStatsView.js
@@ -0,0 +1,35 @@
+import Marionette from "marionette";
+import template from "./page-stats.template.html";
+
+
+export default Marionette.ItemView.extend({
+    template,
+
+    modelEvents: {
+        "change": "render"
+    },
+
+    serializeData: function ()
+    {
+        var data = this.model.toJSON();
+
+        var output = {
+            gameraIdCount: 0,
+            manualIdCount: 0,
+            glyphCount: 0
+        };
+
+        if (data.id_states[0])
+        {
+            output.gameraIdCount = data.id_states[0].id__count;
+            output.glyphCount += data.id_states[0].id__count;
+        }
+        if (data.id_states[1])
+        {
+            output.manualIdCount = data.id_states[1].id__count;
+            output.glyphCount += data.id_states[1].id__count;
+        }
+
+        return output;
+    }
+});
---
+++ public/classifier_frontend/public/js/app/views/PageStats/PageStatsViewModel.js
@@ -0,0 +1,18 @@
+import Backbone from "backbone";
+
+
+export default Backbone.Model.extend({
+    defaults: {
+        id_states: [
+            {
+                id_state_manual: false,
+                id__count: 0
+            },
+            {
+                id_state_manual: true,
+                id__count: 0
+            }
+        ],
+        glyph_count: 0
+    }
+});
