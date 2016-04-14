39d00e7492e0b3ef9947518e3a67fe7933947ebe
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-14 19:25:53 -0400

    New: Latest serializers

--- public/classifier/serializers/glyph.py
+++ public/classifier/serializers/glyph.py
@@ -4,6 +4,17 @@ from rest_framework import serializers
 
 class GlyphSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    # glyph_class = serializers.ReadOnlyField()
+
 
     class Meta:
         model = Glyph
+        fields = ["url", "id", "short_code", "id_state_manual", "confidence", "ulx", "uly", "nrows", "ncols", 'image_file']
+
+
+class GlyphImagelessSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Glyph
+        fields = ["url", "id", "short_code", "id_state_manual", "confidence", "ulx", "uly", "nrows", "ncols"]
--- public/classifier/serializers/page.py
+++ public/classifier/serializers/page.py
@@ -1,6 +1,5 @@
-from classifier.models.page import Page
 from rest_framework import serializers
-
+from classifier.models.page import Page
 from classifier.serializers.glyph import GlyphSerializer
 from classifier.serializers.user import UserSerializer
 
@@ -8,15 +7,28 @@ from classifier.serializers.user import UserSerializer
 class PageFullSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
     glyph_set = GlyphSerializer(many=True, read_only=True)
+    glyph_classes = serializers.ReadOnlyField()
     owner = UserSerializer(read_only=True)
+    # classifier = GameraClassifierMinimalSerializer()
 
     class Meta:
         model = Page
+
+
+class PageMinimalSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    owner = UserSerializer(read_only=True)
+    glyph_classes = serializers.ReadOnlyField()
+    # classifier = GameraClassifierMinimalSerializer()
+
+    class Meta:
+        model = Page
+        fields = ("id", "classifier", "owner", "uuid", "name", "glyph_classes", "width", "height", "image", "binary_image")
 
 
 class PageTeaserSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
     owner = UserSerializer(read_only=True)
+
     class Meta:
         model = Page
-
