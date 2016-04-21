4d0d2b6ea9cbc1eb6121e37155da6ca13eaf3cdd
AFFogarty <andrew.f.fogarty@gmail.com>
2016-04-21 18:06:30 -0400

    New: Interactive classifier successfully loads

--- rodan_job/base.py
+++ rodan_job/base.py
@@ -1,3 +1,5 @@
+import json
+import os
 from aetypes import Enum
 import gamera.core
 import gamera.gamera_xml
@@ -6,12 +8,18 @@ import gamera.knn
 from gamera.core import load_image
 from rodan.jobs.base import RodanTask
 from rodan.jobs.interactive_classifier.intermediary.gamera_xml import GameraXML
+from rodan.settings import MEDIA_URL, MEDIA_ROOT
 
 
 class ClassifierStateEnum(Enum):
-    IMPORT_XML = 1
-    CORRECTION = 2
-    EXPORT_XML = 3
+    IMPORT_XML = 0
+    CORRECTION = 1
+    EXPORT_XML = 2
+
+
+def media_file_path_to_public_url(media_file_path):
+    chars_to_remove = len(MEDIA_ROOT)
+    return os.path.join(MEDIA_URL, media_file_path[chars_to_remove:])
 
 
 class InteractiveClassifier(RodanTask):
@@ -22,10 +30,7 @@ class InteractiveClassifier(RodanTask):
     name = "gamera.custom.interactive_classification"
     author = "Andrew Fogarty"
     description = "A GUI for Gamera interactive kNN classification."
-    settings = {
-        "classifier_state": ClassifierStateEnum.IMPORT_XML,
-        "glyphs": []
-    }
+    settings = {}
     enabled = True
     category = "Classifier"
     interactive = True
@@ -59,6 +64,13 @@ class InteractiveClassifier(RodanTask):
             "minimum": 1,
             "maximum": 1,
             "is_list": False
+        },
+        {
+            "name": "Glyph JSON",
+            "resource_types": ["application/json"],
+            "minimum": 1,
+            "maximum": 1,
+            "is_list": False
         }
     ]
 
@@ -70,14 +82,30 @@ class InteractiveClassifier(RodanTask):
         pass
 
     def get_my_interface(self, inputs, settings):
+        staffless_image_path = inputs['Staffless Image'][0]['resource_path']
+        classifier_path = inputs['Classifier'][0]['resource_path']
         # We need to figure out the best way to include the data in the template
         data = {}
+        data["glyphs"] = json.dumps(GameraXML(classifier_path).get_glyphs())
+        data["binary_image_path"] = media_file_path_to_public_url(staffless_image_path)
         return "interfaces/interactive_classifier.html", data
 
     def run_my_task(self, inputs, settings, outputs):
         # Initialize a gamera classifier
+        staffless_image_path = inputs['Staffless Image'][0]['resource_path']
+        classifier_path = inputs['Classifier'][0]['resource_path']
+        result_path = outputs['Corrected Classifier'][0]['resource_path']
+        # glyph_json_path = outputs['Glyph JSON'][0]['resource_path']
 
-        if settings["classifier_state"] == ClassifierStateEnum.IMPORT_XML:
+        # Set the initial state
+        if "state" not in self.settings:
+            self.settings["state"] = ClassifierStateEnum.IMPORT_XML
+
+        if self.settings["state"] == ClassifierStateEnum.IMPORT_XML:
+            print(settings)
+            self.settings["state"] = ClassifierStateEnum.CORRECTION
+            return self.WAITING_FOR_INPUT()
+        if settings["state"] == ClassifierStateEnum.IMPORT_XML:
             # Convert the GameraXML to an intermediary format
             classifier_path = inputs['Classifier'][0]['resource_path']
 
@@ -106,12 +134,21 @@ class InteractiveClassifier(RodanTask):
                     # Not manually identified, so we will classify it
                     glyphs_to_classify.append(glyph)
 
+            return self.WAITING_FOR_INPUT()
 
-
-
+        #
+        #
+        #
         else:
-            # Encode a new GameraXML file and export it
-            result_path = outputs['Corrected Classifier'][0]['resource_path']
+            # No more corrections are required.  We can now exit!
+            pass
+
 
     def validate_my_user_input(self, inputs, settings, user_input):
-        pass
+        if 'complete' in user_input:
+            # We are complete.  Advance to the final stage
+            self.settings["state"] = ClassifierStateEnum.EXPORT_XML
+        else:
+            # We will prepare for another round of classification
+            pass
+
--- rodan_job/intermediary/gamera_xml.py
+++ rodan_job/intermediary/gamera_xml.py
@@ -1,3 +1,4 @@
+import uuid
 from lxml import etree
 # from classifier.helpers.run_length_image import RunLengthImage
 
@@ -9,15 +10,15 @@ class GameraXML:
 
     def __init__(self, gamera_file_path):
         # Open the file as an etree
-        self.root = etree.parse(open(gamera_file_path, 'r'))
-        self.symbols = self.root[0]
-        self.glyphs = self.root[1]
-
-    def get_class_names(self):
-        output = []
-        for symbol in self.symbols:
-            output.append(symbol.get("name"))
-        return output
+        self.root = etree.parse(open(gamera_file_path, 'r')).getroot()
+        # self.symbols = self.root[0]
+        self.glyphs = self.root[0]
+    #
+    # def get_class_names(self):
+    #     output = []
+    #     for symbol in self.symbols:
+    #         output.append(symbol.get("name"))
+    #     return output
 
     def get_glyphs(self):
         """
@@ -33,17 +34,18 @@ class GameraXML:
             uly = int(glyph.get("uly"))
             print(ulx, uly)
             name = glyph.find("ids").find("id").get("name")
-            run_length_data = glyph.find("data").text
+            run_length_data = glyph.find("data").text.replace('\n', '')
             id_state = glyph.find("ids").get("state")
             confidence = float(glyph.find("ids").find("id").get("confidence"))
             output.append(
                 {
+                    "id": uuid.uuid4().hex,
                     "short_code": name,
                     # "image": RunLengthImage(ulx, uly, width, height,
                     #                         run_length_data),
                     "image": run_length_data,
-                    "width": width,
-                    "height": height,
+                    "ncols": width,
+                    "nrows": height,
                     "ulx": ulx,
                     "uly": uly,
                     "id_state_manual": (id_state == "MANUAL"),
