928ec559630350189a5679a7a1baf3f1f4ba7119
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-04 17:04:01 -0400

    New: Multi edit glyph event

--- public/classifier_frontend/public/js/app/events/GlyphEvents.js
+++ public/classifier_frontend/public/js/app/events/GlyphEvents.js
@@ -2,5 +2,6 @@ export default {
     openGlyphEdit: "open:glyphEdit",
     clickGlyph: "click:glyph",
     moveGlyph: "move:glyph",
-    changeGlyph: "change:glyph"
+    changeGlyph: "change:glyph",
+    dragSelect: "open:multiEdit"
 }
