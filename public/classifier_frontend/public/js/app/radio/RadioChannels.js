682d50ed4d572f8ae0839169b237cfd748c8babd
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-04 17:03:46 -0400

    New: Menu radio channel

--- public/classifier_frontend/public/js/app/radio/RadioChannels.js
+++ public/classifier_frontend/public/js/app/radio/RadioChannels.js
@@ -4,5 +4,6 @@ import Radio from "backbone.radio";
 export default {
     modal: Radio.channel("modal"),
     globalUI: Radio.channel("globalUI"),
-    edit: Radio.channel("edit")
+    edit: Radio.channel("edit"),
+    menu: Radio.channel("menu")
 }
