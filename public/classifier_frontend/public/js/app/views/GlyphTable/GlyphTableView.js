fdecc11dbae79b16fb32431a42b6ed0c0c6da06c
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-05 15:54:15 -0400

    New: Drag selection box

--- public/classifier_frontend/public/js/app/views/GlyphTable/GlyphTableView.js
+++ public/classifier_frontend/public/js/app/views/GlyphTable/GlyphTableView.js
@@ -13,22 +13,18 @@ export default Marionette.CollectionView.extend({
     childView: GlyphTableRowView,
 
     isMouseDown: false,
-    mouseDownX: undefined,
-    mouseDownY: undefined,
+    mouseDownX: 0,
+    mouseDownY: 0,
+    selectionBox: undefined,
+    resizeEvent: undefined,
 
     ui: {
         "selectionBox": ".selection-box"
     },
 
     events: {
-        "keypress .table": "test",
         "mousedown": "onMouseDown",
-        "mouseup": "onMouseUp"
-    },
-
-    test: function (e)
-    {
-        console.log(e);
+        // "mouseup": "onMouseUp"
     },
 
     onMouseDown: function (event)
@@ -37,29 +33,42 @@ export default Marionette.CollectionView.extend({
         this.isMouseDown = true;
         this.mouseDownX = event.clientX;
         this.mouseDownY = event.clientY;
+
+        this.selectionBox.style.top = this.mouseDownY + "px";
+        this.selectionBox.style.left = this.mouseDownX + "px";
+        this.selectionBox.style.width = "0px";
+        this.selectionBox.style.height = "0px";
+        this.selectionBox.style.visibility = "visible";
     },
 
     onMouseUp: function (event)
     {
-        // console.log(event);
-        this.isMouseDown = false;
-        var x = event.clientX,
-            y = event.clientY;
+        console.log("onMouseUp()");
+        if (this.isMouseDown === true)
+        {
+            // console.log(event);
+            this.isMouseDown = false;
+            var x = event.clientX,
+                y = event.clientY;
 
-        var that = this;
-        if (x - this.mouseDownX !== 0 && y - this.mouseDownY !== 0) {
-            var boundingBox = {
-                left: Math.min(that.mouseDownX, x),
-                top: Math.min(that.mouseDownY, y),
-                right: Math.max(that.mouseDownX, x),
-                bottom: Math.max(that.mouseDownY, y)
-            };
+            var that = this;
+            if (x - this.mouseDownX !== 0 && y - this.mouseDownY !== 0) {
+                var boundingBox = {
+                    left: Math.min(that.mouseDownX, x),
+                    top: Math.min(that.mouseDownY, y),
+                    right: Math.max(that.mouseDownX, x),
+                    bottom: Math.max(that.mouseDownY, y)
+                };
 
-            // Empty the previous selection
-            var collection = this.model.get("selection");
-            collection.reset();
-            RadioChannels.edit.trigger(GlyphEvents.dragSelect, boundingBox, collection);
+                // Empty the previous selection
+                var collection = this.model.get("selection");
+                collection.reset();
+                RadioChannels.edit.trigger(GlyphEvents.dragSelect, boundingBox, collection);
+            }
         }
+
+        // Delete the selection box from the DOM
+        this.selectionBox.style.visibility = "hidden";
     },
 
     initialize: function ()
@@ -81,6 +90,9 @@ export default Marionette.CollectionView.extend({
                 viewModel.activate();
                 prevViewModelSet = [viewModel]
             });
+
+        // this.selectionBox = document.createElement("div");
+
     },
 
     childViewOptions: function ()
@@ -98,5 +110,46 @@ export default Marionette.CollectionView.extend({
             collection.push(view.model.get("glyphs"));
         });
         return collection;
+    },
+
+    onShow: function ()
+    {
+        this.selectionBox = document.body.appendChild(document.createElement("div"));
+        this.selectionBox.style.background = "#337ab7";
+        this.selectionBox.style.position = "absolute";
+        this.selectionBox.style.opacity = 0.4;
+        this.selectionBox.style.filter = "alpha(opacity=40)"; // IE8
+        this.selectionBox.style.visibility = "hidden";
+
+        var that = this;
+        $(document).mousemove(function(event)
+        {
+            if (that.isMouseDown === true)
+            {
+                if (event.buttons === 0)
+                {
+                    // that.isMouseDown = false;
+                    // that.selectionBox.style.visibility = "hidden";
+
+                    that.onMouseUp(event);
+                }
+                else
+                {
+                    // console.log("Mousemove!", event);
+                    var x = event.pageX,
+                        y = event.pageY;
+
+                    that.selectionBox.style.left = Math.min(x, that.mouseDownX) + "px";
+                    that.selectionBox.style.top = Math.min(y, that.mouseDownY) + "px";
+                    that.selectionBox.style.width = Math.abs(x - that.mouseDownX) + "px";
+                    that.selectionBox.style.height = Math.abs(y - that.mouseDownY) + "px";
+                }
+            }
+        });
+    },
+
+    onDestroy: function ()
+    {
+        this.seelectionBox.remove();
     }
 });
