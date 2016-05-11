12570f1f8eb5119eaee1d5945d619864a00da65c
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-11 16:29:11 -0400

    New: Sanitize short code changes

--- public/classifier_frontend/public/js/app/models/Glyph.js
+++ public/classifier_frontend/public/js/app/models/Glyph.js
@@ -1,10 +1,11 @@
 import Backbone from "backbone";
 import GlyphEvents from "events/GlyphEvents";
 import RadioChannels from "radio/RadioChannels";
+import ShortCodeUtils from "utils/ShortCodeUtils";
 
 export default Backbone.Model.extend({
 
-    url: function()
+    url: function ()
     {
         return this.get("url");
     },
@@ -23,18 +24,21 @@ export default Backbone.Model.extend({
         context_thumbnail: ""
     },
 
-    changeClass: function(newShortCode)
+    changeClass: function (newShortCode)
     {
         var oldShortCode = this.get("short_code");
 
+        // do the sanitization step
+        var sanitizedShortCode = ShortCodeUtils.sanitizeShortCode(newShortCode);
+
         this.set({
-            short_code: newShortCode,
+            short_code: sanitizedShortCode,
             id_state_manual: true,
             confidence: 1.0
         });
 
         // Update glyph table location
-        RadioChannels.edit.trigger(GlyphEvents.moveGlyph, this, oldShortCode, newShortCode);
+        RadioChannels.edit.trigger(GlyphEvents.moveGlyph, this, oldShortCode, this.get("short_code"));
         RadioChannels.edit.trigger(GlyphEvents.changeGlyph, this);
     }
 });
