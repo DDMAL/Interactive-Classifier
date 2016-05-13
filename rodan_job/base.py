bcc4bc4f7170aaf8f2cf9943c040078984ec8e88
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-13 15:14:12 -0400

    New: Better port names

--- rodan_job/base.py
+++ rodan_job/base.py
@@ -30,17 +30,17 @@ def get_manual_glyphs(glyphs):
     # Prepare the training glyphs
     training_glyphs = []
     for glyph in glyphs:
-        if glyph["id_state_manual"] == True:
+        if glyph['id_state_manual'] == True:
             # Get the gamera image
             gamera_image = RunLengthImage(
-                glyph["ulx"],
-                glyph["uly"],
-                glyph["ncols"],
-                glyph["nrows"],
-                glyph["image"]
+                glyph['ulx'],
+                glyph['uly'],
+                glyph['ncols'],
+                glyph['nrows'],
+                glyph['image']
             ).get_gamera_image()
             # It's a training glyph!
-            gamera_image.classify_manual(glyph["short_code"])
+            gamera_image.classify_manual(glyph['short_code'])
             training_glyphs.append(gamera_image)
     return training_glyphs
 
@@ -65,14 +65,14 @@ def output_corrected_data(cknn, glyphs, output_path):
     output_images = []
     for glyph in glyphs:
         gamera_image = RunLengthImage(
-            glyph["ulx"],
-            glyph["uly"],
-            glyph["ncols"],
-            glyph["nrows"],
-            glyph["image"]
+            glyph['ulx'],
+            glyph['uly'],
+            glyph['ncols'],
+            glyph['nrows'],
+            glyph['image']
         ).get_gamera_image()
-        if glyph["id_state_manual"]:
-            gamera_image.classify_manual(glyph["short_code"])
+        if glyph['id_state_manual']:
+            gamera_image.classify_manual(glyph['short_code'])
         else:
             cknn.classify_glyph_automatic(gamera_image)
         output_images.append(gamera_image)
@@ -87,13 +87,15 @@ def run_output_stage(cknn, glyphs, outputs):
     """
     The job is complete, so save the results to disk.
     """
-    print("Running output stage!")
-    output_training_classifier_path = outputs['Training Classifier'][0]['resource_path']
-    output_classified_data_path = outputs['Classified Data'][0]['resource_path']
+    print('Running output stage!')
+    output_training_classifier_path = outputs['GameraXML - Training Data'][0][
+        'resource_path']
+    output_classified_data_path = outputs['GameraXML - Classified Glyphs'][0][
+        'resource_path']
     # Save the outputs to disk
     output_training_data(cknn, output_training_classifier_path)
     output_corrected_data(cknn, glyphs, output_classified_data_path)
-    print("Complete!")
+    print('Complete!')
 
 
 def prepare_classifier(training_database, glyphs, features_file_path):
@@ -121,24 +123,24 @@ def run_correction_stage(settings, training_database, features_file_path):
     """
     # Train a classifier
     cknn = prepare_classifier(training_database,
-                                   settings["glyphs"],
-                                   features_file_path)
+                              settings['glyphs'],
+                              features_file_path)
     # The automatic classifications
-    for glyph in settings["glyphs"]:
-        if not glyph["id_state_manual"]:
+    for glyph in settings['glyphs']:
+        if not glyph['id_state_manual']:
             # It's a glyph to be classified
             gamera_glyph = RunLengthImage(
-                glyph["ulx"],
-                glyph["uly"],
-                glyph["ncols"],
-                glyph["nrows"],
-                glyph["image"]
+                glyph['ulx'],
+                glyph['uly'],
+                glyph['ncols'],
+                glyph['nrows'],
+                glyph['image']
             ).get_gamera_image()
             # Classify it!
             cknn.classify_glyph_automatic(gamera_glyph)
             # Save the classification back into memory
             result, confidence = cknn.guess_glyph_automatic(gamera_glyph)
-            glyph["short_code"] = result[0][1]
+            glyph['short_code'] = result[0][1]
     return cknn
 
 
@@ -148,7 +150,7 @@ def run_import_stage(settings, classifier_path):
     to the settings dictionary.
     """
     # Extract the glyphs from the Gamera XML file
-    settings["glyphs"] = GameraXML(classifier_path).get_glyphs()
+    settings['glyphs'] = GameraXML(classifier_path).get_glyphs()
 
 
 def serialize_glyphs_to_json(settings):
@@ -156,8 +158,8 @@ def serialize_glyphs_to_json(settings):
     Serialize the glyphs as a JSON dict grouped by short code
     """
     output = {}
-    for glyph in settings["glyphs"]:
-        short_code = glyph["short_code"]
+    for glyph in settings['glyphs']:
+        short_code = glyph['short_code']
         if short_code not in output:
             output[short_code] = []
         output[short_code].append(glyph)
@@ -168,7 +170,7 @@ def purge_serialized_json(settings):
     """
     Remove the cached JSON from the settings
     """
-    settings["glyphs_json"] = None
+    settings['glyphs_json'] = None
 
 
 def update_changed_glyphs(settings):
@@ -176,26 +178,26 @@ def update_changed_glyphs(settings):
     Update the glyph objects that have been changed since the last round of classification
     """
     # Build a hash of the changed glyphs by their id
-    changed_glyph_hash = {g["id"]: g for g in settings["@changed_glyphs"]}
+    changed_glyph_hash = {g['id']: g for g in settings['@changed_glyphs']}
     # Loop through all glyphs.  Update if changed.
-    for glyph in settings["glyphs"]:
+    for glyph in settings['glyphs']:
         if not changed_glyph_hash:
             # No more changed glyphs, so break
             break
         else:
             # There are still glyphs to update
-            key = glyph["id"]
+            key = glyph['id']
             # Grab the changed glyph
             if key in changed_glyph_hash:
                 changed_glyph = changed_glyph_hash[key]
                 # Update the Glyph proper
-                glyph["id_state_manual"] = changed_glyph["id_state_manual"]
-                glyph["short_code"] = changed_glyph["short_code"]
-                glyph["confidence"] = changed_glyph["confidence"]
+                glyph['id_state_manual'] = changed_glyph['id_state_manual']
+                glyph['short_code'] = changed_glyph['short_code']
+                glyph['confidence'] = changed_glyph['confidence']
                 # Pop the changed glyph from the hash
                 changed_glyph_hash.pop(key, None)
     # Clear out the @changed_glyphs from the settings...
-    settings["@changed_glyphs"] = []
+    settings['@changed_glyphs'] = []
 
 
 class InteractiveClassifier(RodanTask):
@@ -203,57 +205,57 @@ class InteractiveClassifier(RodanTask):
     # Description
     #############
 
-    name = "Interactive Classifier"
-    author = "Andrew Fogarty"
-    description = "A GUI for Gamera interactive kNN classification."
+    name = 'Interactive Classifier'
+    author = 'Andrew Fogarty'
+    description = 'A GUI for Gamera interactive kNN classification.'
     settings = {}
     enabled = True
-    category = "Gamera - Classification"
+    category = 'Gamera - Classification'
     interactive = True
     input_port_types = [
         {
-            'name': 'Staffless Image',
+            'name': '1-Bit PNG - Preview Image',
             'resource_types': ['image/onebit+png'],
             'minimum': 1,
             'maximum': 1,
             'is_list': False
         },
         {
-            "name": "Training Classifier",
-            "resource_types": ["application/gamera+xml"],
-            "minimum": 0,
-            "maximum": 1,
-            "is_list": False
-        },
-        {
-            "name": "Classifier",
-            "resource_types": ["application/gamera+xml"],
-            "minimum": 1,
-            "maximum": 1,
-            "is_list": False
-        },
-        {
-            'name': 'Feature Selection',
+            'name': 'GameraXML - Training Data',
             'resource_types': ['application/gamera+xml'],
             'minimum': 0,
             'maximum': 1,
-            "is_list": False
+            'is_list': False
+        },
+        {
+            'name': 'GameraXML - Connected Components',
+            'resource_types': ['application/gamera+xml'],
+            'minimum': 1,
+            'maximum': 1,
+            'is_list': False
+        },
+        {
+            'name': 'GameraXML - Feature Selection',
+            'resource_types': ['application/gamera+xml'],
+            'minimum': 0,
+            'maximum': 1,
+            'is_list': False
         }
     ]
     output_port_types = [
         {
-            "name": "Training Classifier",
-            "resource_types": ["application/gamera+xml"],
-            "minimum": 1,
-            "maximum": 1,
-            "is_list": False
+            'name': 'GameraXML - Training Data',
+            'resource_types': ['application/gamera+xml'],
+            'minimum': 1,
+            'maximum': 1,
+            'is_list': False
         },
         {
-            "name": "Classified Data",
-            "resource_types": ["application/gamera+xml"],
-            "minimum": 1,
-            "maximum": 1,
-            "is_list": False
+            'name': 'GameraXML - Classified Glyphs',
+            'resource_types': ['application/gamera+xml'],
+            'minimum': 1,
+            'maximum': 1,
+            'is_list': False
         }
     ]
 
@@ -265,42 +267,45 @@ class InteractiveClassifier(RodanTask):
         pass
 
     def get_my_interface(self, inputs, settings):
-        staffless_image_path = inputs['Staffless Image'][0]['resource_path']
+        staffless_image_path = inputs['1-Bit PNG - Preview Image'][0][
+            'resource_path']
         # We need to figure out the best way to include the data in the template
         data = {
-            "glyphs": settings["glyphs_json"],
-            "binary_image_path": media_file_path_to_public_url(
+            'glyphs': settings['glyphs_json'],
+            'binary_image_path': media_file_path_to_public_url(
                 staffless_image_path)
         }
-        return "interfaces/interactive_classifier.html", data
+        return 'interfaces/interactive_classifier.html', data
 
     def run_my_task(self, inputs, settings, outputs):
         # Initialize a gamera classifier
-        classifier_path = inputs['Classifier'][0]['resource_path']
-        if "Feature Selection" in inputs:
-            features = inputs['Feature Selection'][0]['resource_path']
+        classifier_path = inputs['GameraXML - Connected Components'][0][
+            'resource_path']
+        if 'GameraXML - Feature Selection' in inputs:
+            features = inputs['GameraXML - Feature Selection'][0][
+                'resource_path']
         else:
             features = None
         # Handle importing the optional training classifier
-        if "Training Classifier" in inputs:
+        if 'GameraXML - Training Data' in inputs:
             training_database = glyphs_from_xml(
-                inputs['Training Classifier'][0]['resource_path'])
+                inputs['GameraXML - Training Data'][0]['resource_path'])
         else:
             training_database = []
         # Set the initial state
-        if "@state" not in settings:
-            settings["@state"] = ClassifierStateEnum.IMPORT_XML
-            settings["glyphs"] = []
-        if settings["@state"] == ClassifierStateEnum.IMPORT_XML:
+        if '@state' not in settings:
+            settings['@state'] = ClassifierStateEnum.IMPORT_XML
+            settings['glyphs'] = []
+        if settings['@state'] == ClassifierStateEnum.IMPORT_XML:
             run_import_stage(settings, classifier_path)
-            settings["@state"] = ClassifierStateEnum.CORRECTION
-            settings["glyphs_json"] = serialize_glyphs_to_json(settings)
+            settings['@state'] = ClassifierStateEnum.CORRECTION
+            settings['glyphs_json'] = serialize_glyphs_to_json(settings)
             return self.WAITING_FOR_INPUT()
-        elif settings["@state"] == ClassifierStateEnum.CORRECTION:
+        elif settings['@state'] == ClassifierStateEnum.CORRECTION:
             # Update any changed glyphs
             update_changed_glyphs(settings)
             run_correction_stage(settings, training_database, features)
-            settings["glyphs_json"] = serialize_glyphs_to_json(settings)
+            settings['glyphs_json'] = serialize_glyphs_to_json(settings)
             return self.WAITING_FOR_INPUT()
         else:
             # Update changed glyphs
@@ -308,7 +313,7 @@ class InteractiveClassifier(RodanTask):
             # Do one final classification before quitting
             cknn = run_correction_stage(settings, training_database, features)
             # No more corrections are required.  We can now output the data
-            run_output_stage(cknn, settings["glyphs"], outputs)
+            run_output_stage(cknn, settings['glyphs'], outputs)
             # Remove the JSON string from the database
             purge_serialized_json(settings)
 
@@ -316,11 +321,11 @@ class InteractiveClassifier(RodanTask):
         if 'complete' in user_input:
             # We are complete.  Advance to the final stage
             return {
-                "@state": ClassifierStateEnum.EXPORT_XML,
-                "@changed_glyphs": user_input["glyphs"]
+                '@state': ClassifierStateEnum.EXPORT_XML,
+                '@changed_glyphs': user_input['glyphs']
             }
         else:
             # We are not complete.  Run another correction stage
             return {
-                "@changed_glyphs": user_input["glyphs"]
+                '@changed_glyphs': user_input['glyphs']
             }
