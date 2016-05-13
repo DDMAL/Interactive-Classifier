cd8d16790a7dd8b97d6a7a4f2d15f95d2ec37a7e
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-13 15:20:05 -0400

    New: Dashboard perf optimization

--- public/classifier_frontend/public/js/app/views/RodanDashboard/RodanDashboardView.js
+++ public/classifier_frontend/public/js/app/views/RodanDashboard/RodanDashboardView.js
@@ -14,6 +14,7 @@ import ImagePreviewView from "../ImagePreview/ImagePreviewView";
 import ImagePreviewViewModel from "../ImagePreview/ImagePreviewViewModel";
 import RadioChannels from "../../radio/RadioChannels";
 import template from "./rodan-dashboard.template.html";
+import Timer from "../../utils/Timer";
 
 export default Marionette.LayoutView.extend({
     template,
@@ -27,89 +28,98 @@ export default Marionette.LayoutView.extend({
         modalTestRegion: ".modal-test"
     },
 
-    onShow: function()
+    initialize: function ()
     {
         // Create the preview window
-        var previewView = new ImagePreviewView({
+        this.previewView = new ImagePreviewView({
             model: new ImagePreviewViewModel({
-                backgroundImage: this.model.get("binary_image"),
+                backgroundImage: this.model.get("binaryImage"),
                 width: 500,
                 height: 500
                 // width: this.model.get("width"),
                 // height: this.model.get("height")
             })
         });
-        // Create the preview
-        this.pagePreviewRegion.show(previewView);
 
-        // // // Construct the glyph table data structure
-        var tableRowCollection = new GlyphTableRowCollection();
+        // Construct the glyph table data structure
+        this.tableRowCollection = new GlyphTableRowCollection();
 
         // Glyph Editing Events
         var that = this;
         this.listenTo(RadioChannels.edit, GlyphEvents.openGlyphEdit,
-            function(model)
+            function (model)
             {
-                previewView.highlightGlyph(model);
+                that.previewView.highlightGlyph(model);
                 that.openGlyphEdit(model);
             });
         this.listenTo(RadioChannels.edit, GlyphEvents.moveGlyph,
-            function(glyph, oldShortCode, newShortCode)
+            function (glyph, oldShortCode, newShortCode)
             {
-                tableRowCollection.moveGlyph(glyph, oldShortCode, newShortCode);
+                that.tableRowCollection.moveGlyph(glyph, oldShortCode, newShortCode);
             }
         );
         this.listenTo(RadioChannels.edit, GlyphEvents.dragSelect,
-            function(boundingBox, collection)
+            function (boundingBox, collection)
             {
                 that.openMultiGlyphEdit(collection);
             }
         );
+    },
 
-        var shortCodes = _.uniq(this.collection.pluck("short_code")).sort();
+    onShow: function ()
+    {
+        var timer = new Timer("RodanDashboardView onShow");
+
+        // Create the preview
+        this.pagePreviewRegion.show(this.previewView);
+
+        timer.tick();
+
+        var glyphDictionary = this.model.get("glyphDictionary");
+        var shortCodes = _.keys(glyphDictionary).sort();
+
         // Show the tree
-        var ctvm = new ClassTreeViewModel({
-            short_codes: shortCodes
-        });
-        this.glyphTreeRegion.show(new ClassTreeView({model: ctvm}));
+        this.glyphTreeRegion.show(new ClassTreeView({
+            model: new ClassTreeViewModel({
+                short_codes: shortCodes
+            })
+        }));
+
+        timer.tick();
 
         var glyphCollections = {};
         // Separate the glyphs by their class
         for (var i = 0; i < shortCodes.length; i++)
         {
-            glyphCollections[shortCodes[i]] = new GlyphCollection();
+            glyphCollections[shortCodes[i]] = new GlyphCollection(glyphDictionary[shortCodes[i]]);
         }
 
-        // Add the glyphs to the glyph collections
-        _.each(this.collection.toArray(), function(element)
-        {
-            var shortCode = element.get("short_code");
-            var json = element.toJSON();
-            glyphCollections[shortCode].add(json);
-        });
+        timer.tick("pre-final render");
 
-        // Show the glyph table
-        var glyphTable = new GlyphTableView({
-            collection: tableRowCollection,
-            // Let the collection know what the sprite sheet will be
-            model: new GlyphTableViewModel({
-                spriteSheetUrl: this.model.get("binary_image")
-            })
-        });
-        this.glyphTableRegion.show(glyphTable);
-
-        _.each(shortCodes, function(shortCode)
+        var that = this;
+        _.each(shortCodes, function (shortCode)
         {
             var row = new GlyphTableRowViewModel({
                 short_code: shortCode,
                 glyphs: glyphCollections[shortCode]
             });
-
-            tableRowCollection.add(row);
+            that.tableRowCollection.add(row);
         });
+
+        timer.tick();
+
+        this.glyphTableRegion.show(new GlyphTableView({
+            collection: this.tableRowCollection,
+            // Let the collection know what the sprite sheet will be
+            model: new GlyphTableViewModel({
+                spriteSheetUrl: this.model.get("binaryImage")
+            })
+        }));
+
+        timer.tick("final");
     },
 
-    openGlyphEdit: function(model)
+    openGlyphEdit: function (model)
     {
         console.log("Open glyphedit!", model);
         this.glyphEditRegion.show(new GlyphEditView({
@@ -117,7 +127,7 @@ export default Marionette.LayoutView.extend({
         }));
     },
 
-    openMultiGlyphEdit: function(collection)
+    openMultiGlyphEdit: function (collection)
     {
         this.glyphEditRegion.show(new GlyphMultiEditView({
             collection: collection
