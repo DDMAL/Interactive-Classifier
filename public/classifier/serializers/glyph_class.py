974740093e00250d9ea6e03c849c58b5d53adf71
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-16 16:00:12 -0400

    Fixed: Removed unused Django app

--- public/classifier/serializers/glyph_class.py
+++
@@ -1,9 +0,0 @@
-from classifier.models.glyph_class import GlyphClass
-from rest_framework import serializers
-
-
-class GlyphClassSerializer(serializers.HyperlinkedModelSerializer):
-    name = serializers.ReadOnlyField()
-    class Meta:
-        model = GlyphClass
-        fields = ("name",)
