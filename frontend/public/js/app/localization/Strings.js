ee857a7730c720405b64a72290a1a6f42a8ec2f3
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-17 15:11:44 -0400

    Fixed: Don't break lines on localized strings

--- frontend/public/js/app/localization/Strings.js
+++ frontend/public/js/app/localization/Strings.js
@@ -1,21 +1,10 @@
 export default {
     siteTitle: "Interactive Classifier",
-
-    submissionWarning: "Your manual corrections will be sent back to " +
-    "Rodan for another round of Gamera classification. " +
-    "Once the classification is complete, you can" +
-    " make more manual corrections.",
-
-    finalizeWarning: "Your manual corrections will be sent back to Rodan for a " +
-    "final round of Gamera classification.  The results will be saved, and the " +
-    "job will complete.  <b>You will not be able to do any more" +
-    " manual corrections!</b>",
-
+    submissionWarning: "Your manual corrections will be sent back to Rodan for another round of Gamera classification.  Once the classification is complete, you can make more manual corrections.",
+    finalizeWarning: "Your manual corrections will be sent back to Rodan for a final round of Gamera classification. The results will be saved, and the job will complete. <b>You will not be able to do any more manual corrections!</b>",
     loadingGlyphs: "Loading page glyphs from the server.  This may take some time...",
-
     menuSubmitLabel: "Submit Corrections and Re-Classify",
     menuFinalizeLabel: "Finalize Classification and Save GameraXML",
-
     loadingPage: "Loading Page...",
     submitCorrections: "Submit Corrections...",
     finalizeCorrections: "Finalize Corrections..."
