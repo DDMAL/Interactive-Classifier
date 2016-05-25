066555e9d9a9905e60e671c97de61663cff21478
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-25 13:38:55 -0400

    Fixed: No collection in table viewmodel

--- frontend/public/js/app/views/GlyphTable/GlyphTableViewModel.js
+++ frontend/public/js/app/views/GlyphTable/GlyphTableViewModel.js
@@ -2,7 +2,6 @@ import Backbone from "backbone";
 
 export default Backbone.Model.extend({
     defaults: {
-        spriteSheetUrl: "#",
-        selection: new Backbone.Collection()
+        spriteSheetUrl: "#"
     }
 });
