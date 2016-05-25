045a32322d1bee11686d245bb236d9fd8abed1ca
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-25 10:04:20 -0400

    Fixed: Cleaned up a bunch of dead code

--- frontend/public/js/app/config/GlobalVars.js
+++
@@ -1,5 +0,0 @@
-export default {
-    SITE_URL: "/classifier/",
-    SITE_SUBFOLDER: "/classifier/",
-    STATIC_URL: "/classifier/media/"
-};
--- frontend/public/js/app/views/Welcome/WelcomeView.js
+++
@@ -1,6 +0,0 @@
-import Marionette from "marionette";
-import template from "./welcome.template.html";
-
-export default Marionette.ItemView.extend({
-    template
-});
