48e8efa5b855ac57e873835dbeacd1951cdb62ec
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-05-30 15:43:19 -0400

    Fixed: Removed some more dead code

--- frontend/public/js/app/views/PageList/PageListView.js
+++
@@ -1,7 +0,0 @@
-import Marionette from "marionette";
-import PageListItemView from "views/PageList/PageListItemView";
-
-export default Marionette.CollectionView.extend({
-    childView: PageListItemView,
-    tagName: 'div class="row"'
-});
