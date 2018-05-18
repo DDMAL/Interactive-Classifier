20676aff806f655737f997d507c109417133bd09
Minh Anh Nguyen <minh.a.nguyen@mail.mcgill.ca>
2018-05-18 16:08:02 -0400

    Updated the minified files

--- frontend/public/js/app/views/RodanDashboard/RodanDashboardView.js
+++ frontend/public/js/app/views/RodanDashboard/RodanDashboardView.js
@@ -131,11 +131,12 @@ export default Marionette.LayoutView.extend(
             this.listenTo(RadioChannels.edit, GlyphEvents.setGlyphName,
               function(className)
               {
-                //Don't display the new class if it's a part of a group or a split
+                //Don't add the new class if it's already in the list or if it's a part of a group or a split
                 if (!className.startsWith("_group._part") && !className.startsWith("_split"))
                 {
                   var index = that.model.get('classNames').findIndex(name => name === className);
-                  if (index === -1){
+                  if (index === -1)
+                  {
                     var classNameList = that.model.get('classNames').push(className);
                     classNameList = that.model.get('classNames').sort();
                     that.model.set('classNames', classNameList);
