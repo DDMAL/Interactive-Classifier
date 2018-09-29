7c8338e318ab7b06c5cde5b53cb19aa501028764
Minh Anh Nguyen <minh.a.nguyen@mail.mcgill.ca>
2018-09-29 11:19:47 -0400

    Scroll to a class or subclass when it is clicked or right-clicked

--- frontend/public/js/app/views/ClassTree/RecursiveUnorderedListView.js
+++ frontend/public/js/app/views/ClassTree/RecursiveUnorderedListView.js
@@ -49,6 +49,7 @@ export default Marionette.ItemView.extend(
             // Extract the name from the HTML5 data attribute.
             var className = event.target.dataset.name;
             RadioChannels.edit.trigger(GlyphEvents.clickGlyphName, className);
+            this.scrollToClass(className);
         },
 
         /**
@@ -64,11 +65,11 @@ export default Marionette.ItemView.extend(
 
             //Extract the class name from the HTML5 data attribute
             var className = event.target.dataset.name;
-
             if (className === "UNCLASSIFIED")
             {
                 return;
             }
+            this.scrollToClass(className);
             var renameElem;
             //get the HTML element that corresponds to the class name
             var classList = document.getElementsByClassName("class-name");
@@ -84,7 +85,29 @@ export default Marionette.ItemView.extend(
             {
                 RadioChannels.edit.trigger(ClassEvents.openClassEdit, className);
             }
+        },
 
+        // Scroll to the class name that was clicked on
+        // If it doesn't exist, scroll to its parent class
+        scrollToClass: function(className)
+        {
+            var match;
+            var rows = document.getElementsByClassName("active");
+            for (var i = 0; i < rows.length; i++)
+            {
+                if (rows[i].textContent.startsWith(className))
+                {
+                    match = rows[i].textContent;
+                    break;
+                }
+            }
+            for (i = 0; i < rows.length; i++)
+            {
+                if (rows[i].textContent === match)
+                {
+                    rows[i].scrollIntoView({block: "start"});
+                }
+            }
         },
 
         onDelete: function(className)
