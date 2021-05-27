50b58caa4ef948e26dbd9920466c50b06c980a4e
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-05-27 11:03:12 -0400

    Clarified wait time

--- frontend/public/js/app/App.js
+++ frontend/public/js/app/App.js
@@ -179,7 +179,7 @@ var App = Marionette.Application.extend(
          * The function extracts glyph data, class names, and preview image path data from the HTML page.  The function
          * then deletes those elements.
          *
-         * Next, we initialize the RodanDashboardView.  We wait four seconds (so that the loading screen modal can
+         * Next, we initialize the RodanDashboardView.  We wait three seconds (so that the loading screen modal can
          * successfully open) and then render the view.
          */
         onStart: function () {
